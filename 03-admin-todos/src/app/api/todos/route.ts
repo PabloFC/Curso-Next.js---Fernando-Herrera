03f5ab4f3295e51e4d2be6002d05f1d1e096ab75
PabloFC <pablofc_68@hotmail.com>
2025-10-15 18:37:13 +0200

    validacion yup

--- 03-admin-todos/src/app/api/todos/route.ts
+++ 03-admin-todos/src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup';
 
 export async function GET(request: Request) { 
 
@@ -24,13 +25,26 @@ export async function GET(request: Request) {
   return NextResponse.json(todos);
 }
 
+const postSchema= yup.object({
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
+
 export async function POST(request: Request) { 
 
-  const body=await request.json();
-
-  const todo=await prisma.todo.create({
-    data:body
-  })
-
-  return NextResponse.json(todo);
+  try {
+    const {complete, description}=await postSchema.validate(await request.json())
+  
+    const todo=await prisma.todo.create({
+      data:{
+        complete,
+        description
+      }
+    })
+  
+    return NextResponse.json(todo);
+    
+  } catch (error) {
+    return NextResponse.json(error, {status:400});
+  }
 }

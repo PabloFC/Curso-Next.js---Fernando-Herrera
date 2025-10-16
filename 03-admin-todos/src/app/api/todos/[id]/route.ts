3e3f23a5f1182678e7633665a1596b91f3fb01aa
PabloFC <pablofc_68@hotmail.com>
2025-10-16 18:16:07 +0200

    put added

--- 03-admin-todos/src/app/api/todos/[id]/route.ts
+++ 03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -26,3 +26,32 @@ export async function GET(request: Request, {params}: Segments) {
    return NextResponse.json(todo);
 }
 
+export async function PUT(request: Request, {params}: Segments) {
+
+  const {id} = params;
+
+  const todo=await prisma.todo.findUnique({
+    where:{
+      id
+    }
+  })
+
+  if(!todo){  
+    return NextResponse.json({message:`Todo con id ${id} no encontrado`}, {status:404});
+  }
+
+  const body=await request.json();
+
+  const updatedTodo= await prisma.todo.update({
+    where:{
+      id
+    },
+    data: {...body}
+  })
+
+
+   return NextResponse.json(updatedTodo);
+}
+
+
+
